506cb0d56f42fa8ec6b5607923b16ef1abe041fd
jwchung1470 <BufordStudent@gmail.com>
2017-09-25 22:15:53 -0400

    Finished Welcome Screen

--- U_Dirty_Rat/app/src/main/java/com/example/jay/u_dirty_rat/WelcomeScreen.java
+++ U_Dirty_Rat/app/src/main/java/com/example/jay/u_dirty_rat/WelcomeScreen.java
@@ -1,9 +1,11 @@
 package com.example.jay.u_dirty_rat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -11,6 +13,18 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        //creating button objects to use as action listener.
+        Button loginButton = (Button) findViewById(R.id.LogInButton);
+        Button registerButton = (Button) findViewById(R.id.RegisterButton); // for future.
+
+        //set the listener and intent.
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WelcomeScreen.this, LoginActivity.class));
+            }
+        });
 
     }
 
