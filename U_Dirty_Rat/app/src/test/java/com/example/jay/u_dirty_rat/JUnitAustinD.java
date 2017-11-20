59f835915f509ab9bbaa194d4ea4aa26115c07c5
Austin <aaustin.d@gmail.com>
2017-11-19 23:20:40 -0500

    added jUnit test for contains(String email)

---
+++ U_Dirty_Rat/app/src/test/java/com/example/jay/u_dirty_rat/JUnitAustinD.java
@@ -0,0 +1,30 @@
+package com.example.jay.u_dirty_rat;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * jUnit test for contains(String email) method.
+ */
+
+public class JUnitAustinD {
+    private UserDB db = UserDB.getInstance();
+
+    @Test
+    public void containsTest_isCorrect() throws Exception {
+        String email = "aaustin.d@gmail.com";
+        Assert.assertEquals(true, db.contains(email));
+    }
+
+    @Test
+    public void containsTest_noEmail() throws Exception {
+        String email = "";
+        Assert.assertEquals(false, db.contains(email));
+    }
+
+    @Test
+    public void containsTest_isIncorrect() throws Exception {
+        String email = "aaustind@gmail.com";
+        Assert.assertEquals(false, db.contains(email));
+    }
+}
